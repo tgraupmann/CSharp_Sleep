a63274e343b45a9c7f665144887c2be675b5223f
Timothy Graupmann <tim.graupmann@razer.com>
2020-11-23 11:38:59 -0800

    Initial commit of project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharp_Sleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check CPU Usage");
+            while (true)
+            {
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
